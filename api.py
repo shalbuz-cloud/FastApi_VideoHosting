24ec19c9e37bae513b7eae649b3bf96c195cdc37
shalbuz <sh.mursalov@mail.ru>
2022-06-17 12:20:07 +0300

    async function get & create video, templates

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter, UploadFile, File, Form, BackgroundTasks
-from fastapi.responses import StreamingResponse
+from typing import List
 
-from schemas import GetVideo, Message
+from fastapi import APIRouter, UploadFile, File, Form, BackgroundTasks
+from fastapi.responses import StreamingResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+
+from schemas import GetListVideo
 from models import Video, User
-from services import save_video
+from services import save_video, open_file
 
 router = APIRouter()
+templates = Jinja2Templates(directory='templates')
 
 
 @router.post("/")
@@ -21,12 +26,42 @@ async def create_video(
     )
 
 
-@router.get(
-    '/video/{video_pk}',
-    response_model=GetVideo,
-    responses={404: {'model': Message}}
-)
-async def get_video(video_pk: int):
-    file = await Video.objects.select_related('user').get(pk=video_pk)
-    file_like = open(file.dict().get('file'), mode='rb')
-    return StreamingResponse(file_like, media_type='video/mp4')
+# @router.get('/video/{video_pk}')
+# async def get_video(video_pk: int):
+#     file = await Video.objects.select_related('user').get(pk=video_pk)
+#     file_like = open(file.file, mode='rb')
+#     return StreamingResponse(file_like, media_type='video/mp4')
+
+
+@router.get('/user/{user_pk}', response_model=List[GetListVideo])
+async def get_list_video(user_pk: int) -> Video:
+    video_list = await Video.objects.filter(user=user_pk).all()
+    return video_list
+
+
+@router.get('/index/{video_pk}', response_class=HTMLResponse)
+async def get_video(request: Request, video_pk: int):
+    return templates.TemplateResponse(
+        'index.html', {'request': request, 'path': video_pk}
+    )
+
+
+@router.get('/video/{video_pk}')
+async def get_streaming_video(
+        request: Request, video_pk: int
+) -> StreamingResponse:
+    file, status_code, content_length, headers = await open_file(
+        request, video_pk
+    )
+    response = StreamingResponse(
+        file,
+        media_type='video/mp4',
+        status_code=status_code,
+    )
+
+    response.headers.update({
+        'Accept-Ranges': 'bytes',
+        'Content-Length': str(content_length),
+        **headers,
+    })
+    return response
--- services.py
+++ services.py
@@ -1,8 +1,12 @@
 import shutil
+from typing import IO, Generator
+from pathlib import Path
 from uuid import uuid4
 
-# import aiofiles
+import aiofiles
+import ormar.exceptions
 from fastapi import UploadFile, BackgroundTasks, HTTPException
+from fastapi.requests import Request
 
 from models import Video, User
 from schemas import UploadVideo
@@ -18,16 +22,75 @@ async def save_video(
     # TODO: Создавать каталог для пользователя при регистрации
     file_name = "media/%s_%s.mp4" % (user.id, uuid4())
     if file.content_type == "video/mp4":
-        background_tasks.add_task(write_video, file_name, file)
+        # background_tasks.add_task(write_video, file_name, file)
+        await write_video(file_name, file)
     else:
         raise HTTPException(status_code=418, detail='It isn\'t mp4')
     info = UploadVideo(title=title, description=description)
     return await Video.objects.create(file=file_name, user=user, **info.dict())
 
 
-def write_video(file_name: str, file: UploadFile):
-    # async with aiofiles.open(file_name, 'wb') as buffer:
-    #     data = await file.read()
-    #     await buffer.write(data)
-    with open(file_name, 'wb') as buffer:
-        shutil.copyfileobj(file.file, buffer)
+async def write_video(file_name: str, file: UploadFile):
+    async with aiofiles.open(file_name, 'wb') as buffer:
+        data = await file.read()
+        await buffer.write(data)
+    # with open(file_name, 'wb') as buffer:
+    #     shutil.copyfileobj(file.file, buffer)
+
+
+def ranged(
+        file: IO[bytes],
+        start: int = 0,
+        end: int = None,
+        block_size: int = 8192,
+) -> Generator[bytes, None, None]:
+    consumed = 0
+
+    file.seek(start)
+    while True:
+        data_length = min(block_size, end - start - consumed) \
+            if end else block_size
+        if data_length <= 0:
+            break
+        data = file.read(data_length)
+        if not data:
+            break
+        consumed += data_length
+        yield data
+
+    if hasattr(file, 'close'):
+        file.close()
+
+
+async def open_file(request: Request, video_pk: int) -> tuple:
+    try:
+        file = await Video.objects.get(pk=video_pk)
+    except ormar.exceptions.NoMatch:
+        raise HTTPException(status_code=404, detail='Not found')
+
+    path = Path(file.dict().get('file'))
+    file = path.open('rb')
+    file_size = path.stat().st_size
+
+    content_length = file_size
+    status_code = 200
+    headers = {}
+    content_range = request.headers.get('range')
+
+    if content_length is not None:
+        content_range = content_range.strip().lower()
+        content_ranges = content_range.split('=')[-1]
+        range_start, range_end, *_ = map(
+            str.strip, (content_ranges + '-').split('-')
+        )
+        range_start = max(0, int(range_start)) if range_start else 0
+        range_end = min(file_size - 1, int(range_end)) if range_end \
+            else file_size - 1
+        content_length = (range_end - range_start) + 1
+        file = ranged(file, start=range_start, end=range_end + 1)
+        status_code = 206
+        headers['Content-Range'] = "bytes %s-%s/%s" % (
+            range_start, range_end, file_size
+        )
+
+    return file, status_code, content_length, headers
